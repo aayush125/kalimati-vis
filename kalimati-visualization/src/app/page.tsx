13b84a57204040525b652a68132ef8f5edaf01c3
Aayush Bhatta <16435069+aayush125@users.noreply.github.com>
2024-12-22 12:20:14 +0545

    Next.js project init

---
+++ kalimati-visualization/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <Button>Click me!</Button>
+    </div>
+  );
+}
